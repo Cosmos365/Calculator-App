2a084e22822993f8fb596eb54f03bdfa50f50751
Avi <81462673+Cosmos365@users.noreply.github.com>
2022-09-09 10:31:23 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function add() {
+    var result = Number(document.getElementById("firstnumber").value) + Number(document.getElementById("secondnumber").value);
+    window.alert(result);
+}
+
+function subtract() {
+    var result = Number(document.getElementById("firstnumber").value) - Number(document.getElementById("secondnumber").value);
+    window.alert(result);
+}
+
+function multiply() {
+    var result = Number(document.getElementById("firstnumber").value) * Number(document.getElementById("secondnumber").value);
+    window.alert(result);
+}
+
+function divide() {
+    var result = Number(document.getElementById("firstnumber").value) / Number(document.getElementById("secondnumber").value);
+    window.alert(result);
+}
